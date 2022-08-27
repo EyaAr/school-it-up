967ad40a6769d5d77df2a0c43a4b7269e07c240b
oussama <oussamabensalem422@gmail.com>
2022-08-27 10:41:16 +0200

    front2

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+    id: number;
+    name: string;
+    email: string;
+    matiere: string;
+    phone: string;
+    imageUrl: string;
+    employeeCode: string;
+    classe:string;
+    dateabs:string;
+    heure:string;
+  }
